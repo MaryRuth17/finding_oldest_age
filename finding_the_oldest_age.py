8d41c10ee405b776076bfc897b795d38cb6ef110
Mary Ruth <[maryruth.pesidas.relator@gmail.com]>
2024-10-22 16:48:08 +0800

    adding put more info option

--- finding_the_oldest_age.py
+++ finding_the_oldest_age.py
@@ -8,5 +8,21 @@ while True: #loop for the user info
             age = int(input("Please input the age: "))
             if age <= 0:
                 raise
+
+            print("The name:", name)
+            print("The age:", age)
+
+            while True:  # loop for adding more input
+                add = input("Want to add more input? (yes/no): ")  # to ask another info
+                if add == "yes":
+                    break  # to continue the outer loop
+                if add == "no":
+                    break
+                else:
+                    print("Invalid response")
+
         except:
             print("Error: Invalid input")
+
+        if add == "no":
+            break
